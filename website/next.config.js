36faaaef4d9ebb87a7b550a2ece9a67f181cd56c
Samuel Poulin <samuel-poulin@outlook.com>
2021-09-21 15:22:44 -0400

    Fixed ip

--- website/next.config.js
+++ website/next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   publicRuntimeConfig: {
-    // host: "http://192.168.0.70:6969",
-    host: "http://localhost:6969",
+    host: "http://192.168.0.70:6969",
   },
   reactStrictMode: true,
 };
