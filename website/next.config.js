dfde72631a4a22cd3f273f3e58012cf51b21df50
Samuel Poulin <samuel-poulin@outlook.com>
2021-09-21 03:29:37 -0400

    Fixed comma missing

--- website/next.config.js
+++ website/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   publicRuntimeConfig: {
-    host: 'http://192.168.0.70:6969'
-  }
+    host: "http://192.168.0.70:6969",
+  },
   reactStrictMode: true,
-}
+};
