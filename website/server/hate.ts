ffd5196a4ad482001e5e9c793d1b2d09a9917ac4
Samuel Poulin <samuel-poulin@outlook.com>
2022-07-13 18:18:57 -0400

    Remove dotenv and add env file to docker compose

--- website/server/hate.ts
+++ website/server/hate.ts
@@ -1,8 +1,5 @@
 import "reflect-metadata";
 
-import dotenv from "dotenv";
-dotenv.config();
-
 import Miska from "./miska";
 import container from "./inversify/inversify.config";
 import TYPES from "./inversify/types";
